ad4eeadbef4fe839918ff303e0b8edeb9abeb1d9
Danil <danilnikonenko04@gmail.com>
2023-02-16 11:17:05 +0200

    changed the assignment for English

--- src/helpers/items/English/practicalWorksIM.js
+++ src/helpers/items/English/practicalWorksIM.js
@@ -3,7 +3,8 @@ import calculateDaysLeft from "./../../calculateDaysLeft";
 const PracticalWorksIM = [
   {
     classType: "Практична робота №1 (IM)",
-    textWork: "Читати та перекладати Unit 13-A\nTask 2-7 (з 8 по 10)",
+    textWork:
+      "Читати та перекладати Unit 13-A\nTask 2-7 (з 8 по 10)/nНа парі ми робили : завдання з 1 по 6 (14-16)",
 
     countingDays: calculateDaysLeft("23.02.2023"), // Дата сдачи задания
   },
