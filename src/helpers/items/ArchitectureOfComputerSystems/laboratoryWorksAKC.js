3f7afbf3f950e41748d003cc2e43fd93dffed3f7
Danil6064 <danilnikonenko04@gmail.com>
2023-03-21 10:29:59 +0200

    Added task for admission to laboratory work

--- src/helpers/items/ArchitectureOfComputerSystems/laboratoryWorksAKC.js
+++ src/helpers/items/ArchitectureOfComputerSystems/laboratoryWorksAKC.js
@@ -1,11 +1,11 @@
 import calculateDaysLeft from "./../../calculateDaysLeft";
 
 const LaboratoryWorksAKC = [
-  // {
-  //   classType: "Лабораторна работа №1 (AKC)",
-  //   textWork: "",
-  //   countingDays: calculateDaysLeft("05.01.2023"), // Дата сдачи задания
-  // },
+   {
+     classType: "Лабораторна работа №1 (AKC)",
+     textWork: "Для допуску к ЛБ№1 вам потрібно зробити 3 задачі за номером своєї бригади (самі задачи ви знайдете в нашій телеграм групі Корисні файли -> АКС_02_AVR_2_.doc)",
+     countingDays: calculateDaysLeft("21.03.2023"), // Дата сдачи задания
+   },
 ];
 
 export { LaboratoryWorksAKC };
