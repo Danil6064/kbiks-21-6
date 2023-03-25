efbdb02e197cde7615b220374ece2d51818a8184
Danil6064 <danilnikonenko04@gmail.com>
2023-03-25 09:27:57 +0200

    Update laboratoryWorksInfT.js

--- src/helpers/items/InformationTechnology/laboratoryWorksInfT.js
+++ src/helpers/items/InformationTechnology/laboratoryWorksInfT.js
@@ -1,11 +1,11 @@
 import calculateDaysLeft from "./../../calculateDaysLeft";
 
 const LaboratoryWorksInfT = [
-  // {
-  //   classType: "Лабораторна работа №1 (InfT)",
-  //   textWork: "",
-  //   countingDays: calculateDaysLeft("05.01.2023"), // Дата сдачи задания
-  // },
+   {
+     classType: "Лабораторна работа №1",
+     textWork: "Всі потрібні матеріали знаходяться на дл нуре. Спочатку оберіть свою бригаду в гугл таблицях, а вже потім переходьте до виконаня ЛБ №1",
+     countingDays: calculateDaysLeft("11.06.2023"), // Дата сдачи задания
+   },
 ];
 
 export { LaboratoryWorksInfT };
