ebdfd364ef76fe623e0bfcc88dc813edec8eba8c
Danil6064 <danilnikonenko04@gmail.com>
2023-03-25 09:20:51 +0200

    Update laboratoryWorksAKC.js

--- src/helpers/items/ArchitectureOfComputerSystems/laboratoryWorksAKC.js
+++ src/helpers/items/ArchitectureOfComputerSystems/laboratoryWorksAKC.js
@@ -4,7 +4,7 @@ const LaboratoryWorksAKC = [
    {
      classType: "Лабораторна работа №1 (AKC)",
      textWork: "Для допуску к ЛБ№1 вам потрібно зробити 3 задачі за номером своєї бригади (самі задачи ви знайдете в нашій телеграм групі Корисні файли -> АКС_02_AVR_2_.doc)",
-     countingDays: calculateDaysLeft("21.03.2023"), // Дата сдачи задания
+     countingDays: calculateDaysLeft("18.04.2023"), // Дата сдачи задания
    },
 ];
 
