74eb35fc8ea793b2f47a5f7f1b714578a86d22f0
Danil6064 <danilnikonenko04@gmail.com>
2023-03-31 17:40:56 +0300

    Update laboratoryWorksPro3.js

--- src/helpers/items/ProgrammingPart3/laboratoryWorksPro3.js
+++ src/helpers/items/ProgrammingPart3/laboratoryWorksPro3.js
@@ -1,27 +1,15 @@
+import calculateDaysLeft from "./../../calculateDaysLeft";
+
+function createHyperlink(url, text = "тут") {
+  return `<a href="${https://drive.google.com/file/d/1QfIt_HGDbFxyWkKeHnE0NVqv4uyyWJ-f/view?usp=share_link}">${text}</a>`;
+}
+
 const LaboratoryWorksPro3 = [
    {
      classType: "Лабораторна работа №1",
-     textWork: `Якщо роботу здати до 04.04.2023 то є шанс отримати доп. бал\nВам потрбно зробти Лабораторну роботу №1, матеріали за посиланням - <a href="https://drive.google.com/file/d/1QfIt_HGDbFxyWkKeHnE0NVqv4uyyWJ-f/view?usp=share_link">тут</a>\nЗдати завдання потрібно на дл нуре`,
-     countingDays: calculateDaysLeft("10.06.2023"), // Дата сдачи задания
+     textWork: `Якщо роботу здати до 04.04.2023, то є шанс отримати додатковий бал\nВам потрібно зробити Лабораторну роботу №1. Матеріали за посиланням - ${createHyperlink("https://drive.google.com/file/d/1QfIt_HGDbFxyWkKeHnE0NVqv4uyyWJ-f/view?usp=share_link")}\nЗдати завдання потрібно на дл нуре.`,
+     countingDays: calculateDaysLeft("10.06.2023"), // Дата сдачи завдання
    },
 ];
 
-function renderTextWork(textWork) {
-  return {__html: textWork};
-}
-
-function App() {
-  return (
-    <div>
-      {LaboratoryWorksPro3.map((item, index) => (
-        <div key={index}>
-          <h2>{item.classType}</h2>
-          <p dangerouslySetInnerHTML={renderTextWork(item.textWork)} />
-          <p>{item.countingDays} днів до дедлайну</p>
-        </div>
-      ))}
-    </div>
-  );
-}
-
-export default App;
+export { LaboratoryWorksPro3 };
