4f21fcd134dbf2792d455962a8e55427abb3fc40
Danil6064 <danilnikonenko04@gmail.com>
2023-04-01 08:42:53 +0300

    Update laboratoryWorksPro3.js

--- src/helpers/items/ProgrammingPart3/laboratoryWorksPro3.js
+++ src/helpers/items/ProgrammingPart3/laboratoryWorksPro3.js
@@ -1,14 +1,12 @@
 import calculateDaysLeft from "./../../calculateDaysLeft";
 
-function createHyperlink(url, text = "тут") {
-  return `<a href="${url}">${text}</a>`;
-}
+const link = "<a href='https://www.google.com'>Google</a>";
 
 const LaboratoryWorksPro3 = [
    {
      classType: "Лабораторна работа №1",
-     textWork: `Якщо роботу здати до 04.04.2023, то є шанс отримати додатковий бал\nВам потрібно зробити Лабораторну роботу №1. Матеріали за посиланням ${createHyperlink("https://www.google.com/", "Google")}\nЗдати завдання потрібно на дл нуре.`,
-     countingDays: calculateDaysLeft("10.06.2023"), // Дата сдачи завдання
+     textWork: `Якщо роботу здати до 04.04.2023, то є шанс отримати додатковий бал\nВам потрібно зробити Лабораторну роботу №1. Матеріали за посиланням ${link}\nЗдати завдання потрібно на дл нуре.`,
+     countingDays: calculateDaysLeft("10.06.2023"), // Дата сдачі завдання
    },
 ];
 
