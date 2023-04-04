f8766c837eab84652167a0ad27942f87bf3dc567
Danil6064 <danilnikonenko04@gmail.com>
2023-04-04 18:21:40 +0300

    Update laboratoryWorksPKr.js

--- src/helpers/items/AppliedCryptography/laboratoryWorksPKr.js
+++ src/helpers/items/AppliedCryptography/laboratoryWorksPKr.js
@@ -1,14 +1,15 @@
 import calculateDaysLeft from "./../../calculateDaysLeft";
 
-const lb_1_application_4 = '<a href="" target="_blank"></a>';
+const lb_1_manual = '<a href="https://drive.google.com/file/d/114VnnaQ-x0m-YuStQqW7QMJe7GDB62lK/view?usp=share_link" target="_blank">Методичка</a>';
+const lb_1_manual_2 = '<a href="https://drive.google.com/file/d/1meqytVjG19ln7Qru1p5LHPk9EwSWzxdm/view?usp=share_link" target="_blank">Книга</a>';
 
 const LaboratoryWorksPKr = [
- //  {
-   //  classType: "Лабораторна работа №1 (PKr)",
-    // textWork: <div dangerouslySetInnerHTML={{ __html: ` ${lb_1_manual_1}` }} />,
+   {
+     classType: "Лабораторна работа №1 (PKr)",
+     textWork: <div dangerouslySetInnerHTML={{ __html: `Зробити лб№1 за посиланням, також є книга, яка може допомогти з завданням${lb_1_manual}\n${lb_1_manual_2}` }} />,
 
-    // countingDays: calculateDaysLeft("05.01.2023"), // Дата сдачи задания
-  // },
+     countingDays: calculateDaysLeft("31.05.2023"), // Дата сдачи задания
+   },
 ];
 
 export { LaboratoryWorksPKr };
