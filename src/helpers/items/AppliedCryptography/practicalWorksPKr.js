480e08cf1a30547bc45aa19425ea358172836a5e
Danil6064 <danilnikonenko04@gmail.com>
2023-04-04 18:11:45 +0300

    Update practicalWorksPKr.js

--- src/helpers/items/AppliedCryptography/practicalWorksPKr.js
+++ src/helpers/items/AppliedCryptography/practicalWorksPKr.js
@@ -1,9 +1,11 @@
 import calculateDaysLeft from "./../../calculateDaysLeft";
 
+const lb_1_manual = '<a href="https://drive.google.com/file/d/1nZ1ncyUfQuQHRNDzLZclk9ow8GAZ9OZ5/view?usp=share_link" target="_blank">Завдання (вхідні данні)</a>';
+
 const PracticalWorksPKr = [
    {
      classType: "Практична робота №1",
-     textWork: "Зробіть практичну роботу №1, завдання знаходиться в класрумі",
+     textWork: <div dangerouslySetInnerHTML={{ __html:Зробіть практичну роботу №1, завдання знаходиться за посиланням` ${lb_1_manual}` }} />,
      countingDays: calculateDaysLeft("26.03.2023"), // Дата сдачи задания
    },
 ];
