b7d5d09e96648817225aa87cef0c7c5a8a1b7f76
Danil6064 <danilnikonenko04@gmail.com>
2023-04-04 17:54:31 +0300

    Update laboratoryWorksPKr.js

--- src/helpers/items/AppliedCryptography/laboratoryWorksPKr.js
+++ src/helpers/items/AppliedCryptography/laboratoryWorksPKr.js
@@ -1,11 +1,14 @@
 import calculateDaysLeft from "./../../calculateDaysLeft";
 
+const lb_1_application_4 = '<a href="" target="_blank"></a>';
+
 const LaboratoryWorksPKr = [
-  // {
-  //   classType: "Лабораторна работа №1 (PKr)",
-  //   textWork: "",
-  //   countingDays: calculateDaysLeft("05.01.2023"), // Дата сдачи задания
-  // },
+   {
+     classType: "Лабораторна работа №1 (PKr)",
+     textWork: <div dangerouslySetInnerHTML={{ __html: ` ${lb_1_manual_1}` }} />,
+
+     countingDays: calculateDaysLeft("05.01.2023"), // Дата сдачи задания
+   },
 ];
 
 export { LaboratoryWorksPKr };
