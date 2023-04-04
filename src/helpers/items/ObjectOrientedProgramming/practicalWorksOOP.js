1bc7d96e45da5c50e4dade5b66547a3eefde4298
Danil6064 <danilnikonenko04@gmail.com>
2023-04-04 14:50:15 +0300

    Update practicalWorksOOP.js

--- src/helpers/items/ObjectOrientedProgramming/practicalWorksOOP.js
+++ src/helpers/items/ObjectOrientedProgramming/practicalWorksOOP.js
@@ -1,11 +1,19 @@
 import calculateDaysLeft from "../../calculateDaysLeft";
 
+const pz_1_manual = '<a href="https://drive.google.com/file/d/106p7DwZ12KpGsiigFWw8J3dS1WCM29R0/view?usp=share_link">Пз№1</a>';
+const pz_2_manual = '<a href="https://drive.google.com/file/d/1EUMzQbqGMbpHdltA-Cm_wDJWgYVju0_Y/view?usp=share_link">Пз№2</a>';
+
 const PracticalWorksOOP = [
-  // {
-  //   classType: "Практична робота №1 (OOP)",
-  //   textWork: "Напишіть есе на 150 слів про своє улюблене хобі.",
-  //   countingDays: calculateDaysLeft("04.01.2022"), // Дата сдачи задания
-  // },
+   {
+     classType: "Практична робота №1",
+     textWork:<div dangerouslySetInnerHTML={{ __html: ` Вам потрібно зробити завдання, матеріали знаходяться за посиланням ${pz_1_manual}` }} />,
+     countingDays: calculateDaysLeft("19.04.2023"), // Дата сдачи задания
+   },
+   {
+     classType: "Практична робота №2",
+     textWork:<div dangerouslySetInnerHTML={{ __html: ` Вам потрібно зробити завдання, матеріали знаходяться за посиланням ${pz_2_manual}` }} />,
+     countingDays: calculateDaysLeft("19.04.2023"), // Дата сдачи задания
+   },
 ];
 
 export { PracticalWorksOOP };
