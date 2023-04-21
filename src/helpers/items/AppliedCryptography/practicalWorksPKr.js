293e234cf6785c5734a57136a34291db3df04c25
Danil6064 <danilnikonenko04@gmail.com>
2023-04-21 12:52:18 +0300

    Update practicalWorksPKr.js

--- src/helpers/items/AppliedCryptography/practicalWorksPKr.js
+++ src/helpers/items/AppliedCryptography/practicalWorksPKr.js
@@ -8,6 +8,11 @@ const PracticalWorksPKr = [
      textWork: <div dangerouslySetInnerHTML={{ __html: `Зробіть практичну роботу №1, завдання знаходиться за посиланням ${pz_1_manual}` }} />,
      countingDays: calculateDaysLeft("26.03.2023"), // Дата сдачи задания
    },
+   {
+     classType: "Практична робота №2",
+     textWork: <div dangerouslySetInnerHTML={{ __html: `Зробіть практичну роботу №2, завдання знаходиться в класрумі (потрібно виконати три задачи з "Задачі для самостійного розв’язання")` }} />,
+     countingDays: calculateDaysLeft("26.03.2023"), // Дата сдачи задания
+   },
 ];
 
 export { PracticalWorksPKr };
