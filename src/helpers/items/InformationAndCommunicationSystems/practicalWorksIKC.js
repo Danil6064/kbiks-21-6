70910dc87a3384ce5c5953be611871c578af2934
Danil6064 <danilnikonenko04@gmail.com>
2023-04-25 17:11:41 +0300

    Update practicalWorksIKC.js

--- src/helpers/items/InformationAndCommunicationSystems/practicalWorksIKC.js
+++ src/helpers/items/InformationAndCommunicationSystems/practicalWorksIKC.js
@@ -1,11 +1,26 @@
 import calculateDaysLeft from "./../../calculateDaysLeft";
 
 const PracticalWorksIKC = [
-  // {
-  //   classType: "Практична робота №1 (IKS)",
-  //   textWork: "Напишіть есе на 150 слів про своє улюблене хобі.",
-  //   countingDays: calculateDaysLeft("04.01.2022"), // Дата сдачи задания
-  // },
+   {
+     classType: "Практична робота №1",
+     textWork: "Потрібно зробити ПЗ№1",
+     countingDays: calculateDaysLeft("31.06.2023"), // Дата сдачи задания
+   },
+   {
+     classType: "Практична робота №2",
+     textWork: "Потрібно зробити ПЗ№2",
+     countingDays: calculateDaysLeft("31.06.2023"), // Дата сдачи задания
+   },
+     {
+     classType: "Практична робота №3",
+     textWork: "Потрібно зробити ПЗ№3",
+     countingDays: calculateDaysLeft("31.06.2023"), // Дата сдачи задания
+   },
+     {
+     classType: "Практична робота №4",
+     textWork: "Потрібно зробити ПЗ№4",
+     countingDays: calculateDaysLeft("31.06.2023"), // Дата сдачи задания
+   },
 ];
 
 export { PracticalWorksIKC };
