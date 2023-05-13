498b2afcf09dd9354b51eda0b9fc56a79d0906d8
Danil6064 <danilnikonenko04@gmail.com>
2023-05-13 07:29:21 +0300

    Update practicalWorksTZI.js

--- src/helpers/items/ComplexesOfTZI/practicalWorksTZI.js
+++ src/helpers/items/ComplexesOfTZI/practicalWorksTZI.js
@@ -4,17 +4,17 @@ const PracticalWorksTZI = [
    {
      classType: "Практична робота №2",
      textWork: "Зробити пз№2 та надіслати в класрумі",
-     countingDays: calculateDaysLeft("04.05.2022"), // Дата сдачи задания
+     countingDays: calculateDaysLeft("04.05.2023"), // Дата сдачи задания
    },
    {
      classType: "Практична робота №3",
      textWork: "Зробити пз№3 та надіслати в класрумі",
-     countingDays: calculateDaysLeft("04.05.2022"), // Дата сдачи задания
+     countingDays: calculateDaysLeft("04.05.2023"), // Дата сдачи задания
    },
    {
      classType: "Практична робота №4",
      textWork: "Зробити пз№4 та надіслати в класрумі",
-     countingDays: calculateDaysLeft("15.05.2022"), // Дата сдачи задания
+     countingDays: calculateDaysLeft("15.05.2023"), // Дата сдачи задания
    },
 ];
 
