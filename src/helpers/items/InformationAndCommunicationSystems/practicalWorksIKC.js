bd2abe348567dc33c91a1c03170c034454de944f
Danil6064 <danilnikonenko04@gmail.com>
2023-05-17 11:10:34 +0300

    Update practicalWorksIKC.js

--- src/helpers/items/InformationAndCommunicationSystems/practicalWorksIKC.js
+++ src/helpers/items/InformationAndCommunicationSystems/practicalWorksIKC.js
@@ -16,7 +16,7 @@ const PracticalWorksIKC = [
      textWork: "Потрібно зробити ПЗ№3",
      countingDays: calculateDaysLeft("30.06.2023"), // Дата сдачи задания
    },
-     {
+   {
      classType: "Практична робота №4",
      textWork: "Потрібно зробити ПЗ№4",
      countingDays: calculateDaysLeft("30.06.2023"), // Дата сдачи задания
