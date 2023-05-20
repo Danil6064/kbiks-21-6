3bd6b3e7527760d377847a88f42fcc489b5538ab
Danil6064 <danilnikonenko04@gmail.com>
2023-05-20 15:24:43 +0300

    Update practicalWorksInfT.js

--- src/helpers/items/InformationTechnology/practicalWorksInfT.js
+++ src/helpers/items/InformationTechnology/practicalWorksInfT.js
@@ -1,11 +1,21 @@
 import calculateDaysLeft from "./../../calculateDaysLeft";
 
 const PracticalWorksInfT = [
-  // {
-  //   classType: "Практична робота №1 (InfT)",
-  //   textWork: "Напишіть есе на 150 слів про своє улюблене хобі.",
-  //   countingDays: calculateDaysLeft("04.01.2022"), // Дата сдачи задания
-  // },
+   {
+     classType: "Практична робота №1",
+     textWork: "Потрібно зробити пз№1, завдання знаходиться на дл нуре",
+     countingDays: calculateDaysLeft("11.06.2023"), // Дата сдачи задания
+   },
+   {
+     classType: "Практична робота №2",
+     textWork: "Потрібно зробити пз№2, завдання знаходиться на дл нуре",
+     countingDays: calculateDaysLeft("11.06.2023"), // Дата сдачи задания
+   },
+   {
+     classType: "Практична робота №3",
+     textWork: "Потрібно зробити пз№3, завдання знаходиться на дл нуре",
+     countingDays: calculateDaysLeft("11.06.2023"), // Дата сдачи задания
+   },
 ];
 
 export { PracticalWorksInfT };
