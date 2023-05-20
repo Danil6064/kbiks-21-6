eef604bbff49b96b10b6d19ec3aab17354b1e693
Danil6064 <danilnikonenko04@gmail.com>
2023-05-20 15:48:03 +0300

    Update laboratoryWorksIKC.js

--- src/helpers/items/InformationAndCommunicationSystems/laboratoryWorksIKC.js
+++ src/helpers/items/InformationAndCommunicationSystems/laboratoryWorksIKC.js
@@ -7,18 +7,18 @@ const LaboratoryWorksIKC = [
      countingDays: calculateDaysLeft("30.06.2023"), // Дата сдачи задания
    },
    {
-     classType: "Лабораторна работа №1",
-     textWork: "Потрібно зробити лб№1",
+     classType: "Лабораторна работа №2",
+     textWork: "Потрібно зробити лб№2",
      countingDays: calculateDaysLeft("30.06.2023"), // Дата сдачи задания
    },
    {
-     classType: "Лабораторна работа №1",
-     textWork: "Потрібно зробити лб№1",
+     classType: "Лабораторна работа №3",
+     textWork: "Потрібно зробити лб№3",
      countingDays: calculateDaysLeft("30.06.2023"), // Дата сдачи задания
    },
    {
-     classType: "Лабораторна работа №1",
-     textWork: "Потрібно зробити лб№1",
+     classType: "Лабораторна работа №4",
+     textWork: "Потрібно зробити лб№4",
      countingDays: calculateDaysLeft("30.06.2023"), // Дата сдачи задания
    },
 ];
