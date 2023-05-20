575357fb1f08cb305bdaddb269e23790e1fc9bbb
Danil6064 <danilnikonenko04@gmail.com>
2023-05-20 15:28:50 +0300

    Update practicalWorksPro3.js

--- src/helpers/items/ProgrammingPart3/practicalWorksPro3.js
+++ src/helpers/items/ProgrammingPart3/practicalWorksPro3.js
@@ -6,6 +6,11 @@ const PracticalWorksPro3 = [
      textWork: "Вам потрібно зробити тест",
      countingDays: calculateDaysLeft("12.04.2023"), // Дата сдачи задания
    },
+   {
+     classType: "Тестова робота №2",
+     textWork: "Вам потрібно зробити тест",
+     countingDays: calculateDaysLeft("27.05.2023"), // Дата сдачи задания
+   },
 ];
 
 export { PracticalWorksPro3 };
